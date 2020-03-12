978815e6a8d87de23c4d14084e8eb4c73506053e
caseyanderson <cta@caseyanderson.com>
2020-03-12 16:39:38 -0700

    messages are now sent on keyDown, not on keyUp

--- scripts/keyboard_sender.py
+++ scripts/keyboard_sender.py
@@ -29,12 +29,12 @@ trigCount = 0
 
 while True:
     if keyboard.is_pressed('Space') and keyPress == False:
-        keyPress = True
-    elif keyboard.is_pressed('Space') == False and keyPress == True:
         trigCount+=1
         print(''.join(["trig count: ", str(trigCount)]))
         msg[0] = random.randint(1, 12)
         client.send(msg)
+        keyPress = True
+    elif keyboard.is_pressed('Space') == False and keyPress == True:
         keyPress = False
     elif keyboard.is_pressed('q') == True:
         print("goodbye!")
